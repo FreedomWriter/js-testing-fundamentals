92be55162a8087b6bf337562426aa5a1825e0c64
FreedomWriter <natalienetworks@gmail.com>
2020-10-19 17:55:20 -0500

    npm i

--- lessons/assertion-library.js
+++ lessons/assertion-library.js
@@ -10,10 +10,31 @@ result = subtract(7, 3)
 expected = 4
 expect(result).toBe(expected)
 
+function sumTest() {
+  result = sum(3,7)
+  expected=10
+  expect(result).toBe(expected)
+}
+
+function subtractTest(params) {
+
+}
+
+function test(title, callback) {
+  try {
+    callback()
+    console.log(`CHECK ${title}`)
+  } catch (error) {
+    console.error(`x ${title}`)
+console.error(error)
+  }
+}
+
+
 function expect(actual) {
   return {
-    toBe(expected) {
-      if (actual !== expected) {
+    toBe(expected){
+      if (actual !== expeccted) {
         throw new Error(`${actual} is not equal to ${expected}`)
       }
     }
